0c90f1f280a4eccda4bb047fcfd971950320fe9d
Abdullah H.M <84979188+abdullah-hm@users.noreply.github.com>
2023-06-19 06:37:18 +0530

    Create AdminLogin.Designer.cs

---
+++ CPMS/AdminLogin.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace CPMS
+{
+    partial class admin_login_groupbox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.admin_groupbox = new System.Windows.Forms.GroupBox();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.lbl_password = new System.Windows.Forms.Label();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.lbl_username = new System.Windows.Forms.Label();
+            this.admin_groupbox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // admin_groupbox
+            // 
+            this.admin_groupbox.Controls.Add(this.btn_clear);
+            this.admin_groupbox.Controls.Add(this.btn_login);
+            this.admin_groupbox.Controls.Add(this.txt_password);
+            this.admin_groupbox.Controls.Add(this.lbl_password);
+            this.admin_groupbox.Controls.Add(this.txt_username);
+            this.admin_groupbox.Controls.Add(this.lbl_username);
+            this.admin_groupbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.admin_groupbox.ForeColor = System.Drawing.Color.Aqua;
+            this.admin_groupbox.Location = new System.Drawing.Point(12, 12);
+            this.admin_groupbox.Name = "admin_groupbox";
+            this.admin_groupbox.Size = new System.Drawing.Size(353, 353);
+            this.admin_groupbox.TabIndex = 0;
+            this.admin_groupbox.TabStop = false;
+            this.admin_groupbox.Text = "A D M I N    L O G I N";
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.BackColor = System.Drawing.Color.Yellow;
+            this.btn_clear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clear.ForeColor = System.Drawing.Color.Black;
+            this.btn_clear.Location = new System.Drawing.Point(32, 274);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(287, 35);
+            this.btn_clear.TabIndex = 6;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = false;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // btn_login
+            // 
+            this.btn_login.BackColor = System.Drawing.Color.Lime;
+            this.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_login.ForeColor = System.Drawing.Color.Black;
+            this.btn_login.Location = new System.Drawing.Point(32, 222);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(287, 35);
+            this.btn_login.TabIndex = 5;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = false;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // txt_password
+            // 
+            this.txt_password.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.txt_password.ForeColor = System.Drawing.Color.White;
+            this.txt_password.Location = new System.Drawing.Point(32, 160);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(287, 26);
+            this.txt_password.TabIndex = 4;
+            this.txt_password.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_password.UseSystemPasswordChar = true;
+            // 
+            // lbl_password
+            // 
+            this.lbl_password.AutoSize = true;
+            this.lbl_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_password.ForeColor = System.Drawing.SystemColors.Window;
+            this.lbl_password.Location = new System.Drawing.Point(28, 137);
+            this.lbl_password.Name = "lbl_password";
+            this.lbl_password.Size = new System.Drawing.Size(91, 20);
+            this.lbl_password.TabIndex = 3;
+            this.lbl_password.Text = "Password:";
+            // 
+            // txt_username
+            // 
+            this.txt_username.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.txt_username.ForeColor = System.Drawing.Color.White;
+            this.txt_username.Location = new System.Drawing.Point(32, 82);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(287, 26);
+            this.txt_username.TabIndex = 2;
+            this.txt_username.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lbl_username
+            // 
+            this.lbl_username.AutoSize = true;
+            this.lbl_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_username.ForeColor = System.Drawing.SystemColors.Window;
+            this.lbl_username.Location = new System.Drawing.Point(28, 59);
+            this.lbl_username.Name = "lbl_username";
+            this.lbl_username.Size = new System.Drawing.Size(103, 20);
+            this.lbl_username.TabIndex = 1;
+            this.lbl_username.Text = "User Name:";
+            // 
+            // admin_login_groupbox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(377, 377);
+            this.Controls.Add(this.admin_groupbox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "admin_login_groupbox";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.admin_login_groupbox_Load);
+            this.admin_groupbox.ResumeLayout(false);
+            this.admin_groupbox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox admin_groupbox;
+        private System.Windows.Forms.Label lbl_username;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.Label lbl_password;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_clear;
+    }
+}
