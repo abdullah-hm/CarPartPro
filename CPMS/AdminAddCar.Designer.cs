8c607d0f0a2e652061d1b4af5b8eb0a66bd12fe1
Abdullah H.M <84979188+abdullah-hm@users.noreply.github.com>
2023-06-19 06:34:55 +0530

    Create AdminAddCar.Designer.cs

---
+++ CPMS/AdminAddCar.Designer.cs
@@ -0,0 +1,329 @@
+﻿
+namespace CPMS
+{
+    partial class AdminAddCar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.admin_add_car_groupbox = new System.Windows.Forms.GroupBox();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.lbl_car_status = new System.Windows.Forms.Label();
+            this.txt_car_price = new System.Windows.Forms.TextBox();
+            this.txt_car_brand = new System.Windows.Forms.TextBox();
+            this.txt_car_name = new System.Windows.Forms.TextBox();
+            this.lbl_car_type = new System.Windows.Forms.Label();
+            this.lbl_car_color = new System.Windows.Forms.Label();
+            this.lbl_car_price = new System.Windows.Forms.Label();
+            this.lbl_car_year = new System.Windows.Forms.Label();
+            this.lbl_car_brand = new System.Windows.Forms.Label();
+            this.lbl_car_name = new System.Windows.Forms.Label();
+            this.cbox_car_status = new System.Windows.Forms.ComboBox();
+            this.cbox_car_type = new System.Windows.Forms.ComboBox();
+            this.txt_car_year = new System.Windows.Forms.TextBox();
+            this.cbox_car_color = new System.Windows.Forms.ComboBox();
+            this.admin_add_car_groupbox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // admin_add_car_groupbox
+            // 
+            this.admin_add_car_groupbox.Controls.Add(this.cbox_car_color);
+            this.admin_add_car_groupbox.Controls.Add(this.cbox_car_type);
+            this.admin_add_car_groupbox.Controls.Add(this.cbox_car_status);
+            this.admin_add_car_groupbox.Controls.Add(this.btn_close);
+            this.admin_add_car_groupbox.Controls.Add(this.btn_clear);
+            this.admin_add_car_groupbox.Controls.Add(this.btn_add);
+            this.admin_add_car_groupbox.Controls.Add(this.lbl_car_status);
+            this.admin_add_car_groupbox.Controls.Add(this.txt_car_price);
+            this.admin_add_car_groupbox.Controls.Add(this.txt_car_year);
+            this.admin_add_car_groupbox.Controls.Add(this.txt_car_brand);
+            this.admin_add_car_groupbox.Controls.Add(this.txt_car_name);
+            this.admin_add_car_groupbox.Controls.Add(this.lbl_car_type);
+            this.admin_add_car_groupbox.Controls.Add(this.lbl_car_color);
+            this.admin_add_car_groupbox.Controls.Add(this.lbl_car_price);
+            this.admin_add_car_groupbox.Controls.Add(this.lbl_car_year);
+            this.admin_add_car_groupbox.Controls.Add(this.lbl_car_brand);
+            this.admin_add_car_groupbox.Controls.Add(this.lbl_car_name);
+            this.admin_add_car_groupbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.admin_add_car_groupbox.ForeColor = System.Drawing.Color.Yellow;
+            this.admin_add_car_groupbox.Location = new System.Drawing.Point(13, 12);
+            this.admin_add_car_groupbox.Name = "admin_add_car_groupbox";
+            this.admin_add_car_groupbox.Size = new System.Drawing.Size(554, 396);
+            this.admin_add_car_groupbox.TabIndex = 2;
+            this.admin_add_car_groupbox.TabStop = false;
+            this.admin_add_car_groupbox.Text = "A D M I N    A D D    N E W    C A R";
+            // 
+            // btn_close
+            // 
+            this.btn_close.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btn_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_close.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_close.ForeColor = System.Drawing.Color.Black;
+            this.btn_close.Location = new System.Drawing.Point(20, 336);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(258, 35);
+            this.btn_close.TabIndex = 10;
+            this.btn_close.Text = "Close";
+            this.btn_close.UseVisualStyleBackColor = false;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.BackColor = System.Drawing.Color.Yellow;
+            this.btn_clear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clear.ForeColor = System.Drawing.Color.Black;
+            this.btn_clear.Location = new System.Drawing.Point(284, 336);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(251, 35);
+            this.btn_clear.TabIndex = 9;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = false;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // btn_add
+            // 
+            this.btn_add.BackColor = System.Drawing.Color.Aqua;
+            this.btn_add.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_add.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_add.ForeColor = System.Drawing.Color.Black;
+            this.btn_add.Location = new System.Drawing.Point(20, 281);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(515, 35);
+            this.btn_add.TabIndex = 8;
+            this.btn_add.Text = "Save";
+            this.btn_add.UseVisualStyleBackColor = false;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click_1);
+            // 
+            // lbl_car_status
+            // 
+            this.lbl_car_status.AutoSize = true;
+            this.lbl_car_status.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_car_status.ForeColor = System.Drawing.SystemColors.Window;
+            this.lbl_car_status.Location = new System.Drawing.Point(373, 208);
+            this.lbl_car_status.Name = "lbl_car_status";
+            this.lbl_car_status.Size = new System.Drawing.Size(151, 20);
+            this.lbl_car_status.TabIndex = 7;
+            this.lbl_car_status.Text = "Car Stock Status:";
+            // 
+            // txt_car_price
+            // 
+            this.txt_car_price.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.txt_car_price.ForeColor = System.Drawing.Color.White;
+            this.txt_car_price.Location = new System.Drawing.Point(284, 158);
+            this.txt_car_price.Name = "txt_car_price";
+            this.txt_car_price.Size = new System.Drawing.Size(251, 26);
+            this.txt_car_price.TabIndex = 4;
+            this.txt_car_price.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt_car_brand
+            // 
+            this.txt_car_brand.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.txt_car_brand.ForeColor = System.Drawing.Color.White;
+            this.txt_car_brand.Location = new System.Drawing.Point(284, 82);
+            this.txt_car_brand.Name = "txt_car_brand";
+            this.txt_car_brand.Size = new System.Drawing.Size(251, 26);
+            this.txt_car_brand.TabIndex = 2;
+            this.txt_car_brand.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt_car_name
+            // 
+            this.txt_car_name.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.txt_car_name.ForeColor = System.Drawing.Color.White;
+            this.txt_car_name.Location = new System.Drawing.Point(20, 82);
+            this.txt_car_name.Name = "txt_car_name";
+            this.txt_car_name.Size = new System.Drawing.Size(245, 26);
+            this.txt_car_name.TabIndex = 1;
+            this.txt_car_name.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lbl_car_type
+            // 
+            this.lbl_car_type.AutoSize = true;
+            this.lbl_car_type.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_car_type.ForeColor = System.Drawing.SystemColors.Window;
+            this.lbl_car_type.Location = new System.Drawing.Point(226, 208);
+            this.lbl_car_type.Name = "lbl_car_type";
+            this.lbl_car_type.Size = new System.Drawing.Size(85, 20);
+            this.lbl_car_type.TabIndex = 4;
+            this.lbl_car_type.Text = "Car Type:";
+            // 
+            // lbl_car_color
+            // 
+            this.lbl_car_color.AutoSize = true;
+            this.lbl_car_color.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_car_color.ForeColor = System.Drawing.SystemColors.Window;
+            this.lbl_car_color.Location = new System.Drawing.Point(26, 208);
+            this.lbl_car_color.Name = "lbl_car_color";
+            this.lbl_car_color.Size = new System.Drawing.Size(89, 20);
+            this.lbl_car_color.TabIndex = 5;
+            this.lbl_car_color.Text = "Car Color:";
+            // 
+            // lbl_car_price
+            // 
+            this.lbl_car_price.AutoSize = true;
+            this.lbl_car_price.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_car_price.ForeColor = System.Drawing.SystemColors.Window;
+            this.lbl_car_price.Location = new System.Drawing.Point(280, 135);
+            this.lbl_car_price.Name = "lbl_car_price";
+            this.lbl_car_price.Size = new System.Drawing.Size(121, 20);
+            this.lbl_car_price.TabIndex = 6;
+            this.lbl_car_price.Text = "Car Price(Rs):";
+            // 
+            // lbl_car_year
+            // 
+            this.lbl_car_year.AutoSize = true;
+            this.lbl_car_year.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_car_year.ForeColor = System.Drawing.SystemColors.Window;
+            this.lbl_car_year.Location = new System.Drawing.Point(21, 135);
+            this.lbl_car_year.Name = "lbl_car_year";
+            this.lbl_car_year.Size = new System.Drawing.Size(158, 20);
+            this.lbl_car_year.TabIndex = 3;
+            this.lbl_car_year.Text = "Manufacture Year:";
+            // 
+            // lbl_car_brand
+            // 
+            this.lbl_car_brand.AutoSize = true;
+            this.lbl_car_brand.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_car_brand.ForeColor = System.Drawing.SystemColors.Window;
+            this.lbl_car_brand.Location = new System.Drawing.Point(280, 59);
+            this.lbl_car_brand.Name = "lbl_car_brand";
+            this.lbl_car_brand.Size = new System.Drawing.Size(95, 20);
+            this.lbl_car_brand.TabIndex = 2;
+            this.lbl_car_brand.Text = "Car Brand:";
+            // 
+            // lbl_car_name
+            // 
+            this.lbl_car_name.AutoSize = true;
+            this.lbl_car_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_car_name.ForeColor = System.Drawing.SystemColors.Window;
+            this.lbl_car_name.Location = new System.Drawing.Point(16, 59);
+            this.lbl_car_name.Name = "lbl_car_name";
+            this.lbl_car_name.Size = new System.Drawing.Size(93, 20);
+            this.lbl_car_name.TabIndex = 1;
+            this.lbl_car_name.Text = "Car Name:";
+            // 
+            // cbox_car_status
+            // 
+            this.cbox_car_status.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.cbox_car_status.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbox_car_status.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbox_car_status.ForeColor = System.Drawing.Color.White;
+            this.cbox_car_status.FormattingEnabled = true;
+            this.cbox_car_status.Items.AddRange(new object[] {
+            "in stock",
+            "out stock"});
+            this.cbox_car_status.Location = new System.Drawing.Point(368, 231);
+            this.cbox_car_status.Name = "cbox_car_status";
+            this.cbox_car_status.Size = new System.Drawing.Size(168, 24);
+            this.cbox_car_status.TabIndex = 1;
+            // 
+            // cbox_car_type
+            // 
+            this.cbox_car_type.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.cbox_car_type.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbox_car_type.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbox_car_type.ForeColor = System.Drawing.Color.White;
+            this.cbox_car_type.FormattingEnabled = true;
+            this.cbox_car_type.Items.AddRange(new object[] {
+            "Hybrid",
+            "Petrol",
+            "Diesel",
+            "Electric"});
+            this.cbox_car_type.Location = new System.Drawing.Point(194, 231);
+            this.cbox_car_type.Name = "cbox_car_type";
+            this.cbox_car_type.Size = new System.Drawing.Size(168, 24);
+            this.cbox_car_type.TabIndex = 6;
+            // 
+            // txt_car_year
+            // 
+            this.txt_car_year.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.txt_car_year.ForeColor = System.Drawing.Color.White;
+            this.txt_car_year.Location = new System.Drawing.Point(20, 158);
+            this.txt_car_year.MaxLength = 4;
+            this.txt_car_year.Name = "txt_car_year";
+            this.txt_car_year.Size = new System.Drawing.Size(245, 26);
+            this.txt_car_year.TabIndex = 3;
+            this.txt_car_year.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // cbox_car_color
+            // 
+            this.cbox_car_color.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.cbox_car_color.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cbox_car_color.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbox_car_color.ForeColor = System.Drawing.Color.White;
+            this.cbox_car_color.FormattingEnabled = true;
+            this.cbox_car_color.Items.AddRange(new object[] {
+            "Black",
+            "White",
+            "Blue",
+            "Red",
+            "Green",
+            "Gray"});
+            this.cbox_car_color.Location = new System.Drawing.Point(20, 231);
+            this.cbox_car_color.Name = "cbox_car_color";
+            this.cbox_car_color.Size = new System.Drawing.Size(168, 24);
+            this.cbox_car_color.TabIndex = 5;
+            // 
+            // AdminAddCar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(580, 427);
+            this.Controls.Add(this.admin_add_car_groupbox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "AdminAddCar";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.admin_add_car_groupbox.ResumeLayout(false);
+            this.admin_add_car_groupbox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox admin_add_car_groupbox;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Label lbl_car_status;
+        private System.Windows.Forms.TextBox txt_car_price;
+        private System.Windows.Forms.TextBox txt_car_brand;
+        private System.Windows.Forms.TextBox txt_car_name;
+        private System.Windows.Forms.Label lbl_car_type;
+        private System.Windows.Forms.Label lbl_car_color;
+        private System.Windows.Forms.Label lbl_car_price;
+        private System.Windows.Forms.Label lbl_car_year;
+        private System.Windows.Forms.Label lbl_car_brand;
+        private System.Windows.Forms.Label lbl_car_name;
+        private System.Windows.Forms.ComboBox cbox_car_type;
+        private System.Windows.Forms.ComboBox cbox_car_status;
+        private System.Windows.Forms.TextBox txt_car_year;
+        private System.Windows.Forms.ComboBox cbox_car_color;
+    }
+}
