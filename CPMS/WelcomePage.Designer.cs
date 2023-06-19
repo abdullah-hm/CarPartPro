ee85fcf2eda1c45a755db42fc13cc45073628553
Abdullah Hassan <84979188+abdullah-hm@users.noreply.github.com>
2023-06-19 07:27:10 +0530

    Add files via upload

---
+++ CPMS/WelcomePage.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace CPMS
+{
+    partial class WelcomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WelcomePage));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_admin = new System.Windows.Forms.Button();
+            this.btn_customer = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(608, 421);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_admin
+            // 
+            this.btn_admin.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btn_admin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_admin.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_admin.Location = new System.Drawing.Point(74, 333);
+            this.btn_admin.Name = "btn_admin";
+            this.btn_admin.Size = new System.Drawing.Size(172, 47);
+            this.btn_admin.TabIndex = 1;
+            this.btn_admin.Text = "ADMIN";
+            this.btn_admin.UseVisualStyleBackColor = false;
+            this.btn_admin.Click += new System.EventHandler(this.btn_admin_Click);
+            // 
+            // btn_customer
+            // 
+            this.btn_customer.BackColor = System.Drawing.Color.Tomato;
+            this.btn_customer.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_customer.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_customer.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_customer.Location = new System.Drawing.Point(375, 333);
+            this.btn_customer.Name = "btn_customer";
+            this.btn_customer.Size = new System.Drawing.Size(172, 48);
+            this.btn_customer.TabIndex = 1;
+            this.btn_customer.Text = "CUSTOMER";
+            this.btn_customer.UseVisualStyleBackColor = false;
+            this.btn_customer.Click += new System.EventHandler(this.btn_customer_Click);
+            // 
+            // WelcomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
+            this.ClientSize = new System.Drawing.Size(607, 421);
+            this.Controls.Add(this.btn_customer);
+            this.Controls.Add(this.btn_admin);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "WelcomePage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_admin;
+        private System.Windows.Forms.Button btn_customer;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
---
+++ CPMS/WelcomePage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CPMS
+{
+    public partial class WelcomePage : Form
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_admin_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            admin_login_groupbox al = new admin_login_groupbox();
+            al.Show();
+        }
+
+        private void btn_customer_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            CustomerLogin cl = new CustomerLogin();
+            cl.Show();
+        }
+    }
+}
